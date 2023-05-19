0721034213a5e88a40700b40292f6926d9a3c4e0
askrzypczynska <aleksandraskrzypczynska@gmail.com>
2023-05-19 17:26:29 +0200

    Adding questions

---
+++ src/Components/BazaDanychPytań.js
@@ -0,0 +1,174 @@
+    let animals = [
+        ["Jakiego rodzaju zwierzę to kangur?", "Ssak", "Ptak", "Gad", "Płaz", 2],
+        ["Gdzie żyją pandy?", "Afryka", "Ameryka Południowa", "Australia", "Azja", 4],
+        ["Jakie zwierzę jest największe na Ziemi?", "Słoń", "Rekin Wielorybi", "Żyrafa", "Płetwal Błękitny", 4],
+        ["Jaki jest największy gatunek węża na świecie?", "Kobra królewska", "Pyton siatkowany", "Wąż mamba czarna", "Boa dusiciel", 2],
+        ["Jakie zwierzę ma najdłuższy okres życia?", "Żółw morski", "Wieloryb", "Słoń afrykański", "Orka", 1],
+        ["Jak nazywa się największy ptak na świecie?", "Orzeł", "Struś", "Albatros", "Kondor andyjski", 2],
+        ["Które zwierzę nie ma zębów?", "Tygrys", "Struś", "Krokodyl", "Hipopotam", 2],
+        ["Jak nazywa się najmniejszy gatunek ssaka na świecie?", "Mysz domowa", "Nietoperz", "Muszka owocowa", "Ryjówka malutka", 4],
+        ["Gdzie żyją lamparty?", "Ameryka Północna", "Europa", "Afryka", "Australia", 3],
+        ["Jaki jest największy gatunek pingwina?", "Pingwin cesarski", "Pingwin królewski", "Pingwin adeli", "Pingwin Galapagos", 2],
+
+    ]
+
+    let programing = [
+        ["Jaki język programowania jest najczęściej wykorzystywany do tworzenia aplikacji mobilnych na system Android?", "Swift", "Java", "C#", "Python", 2],
+        ["Co oznacza skrót API w kontekście programowania?", "Automated Program Installation", "Application Programming Interface", "Advanced Programming Instruction", "All-Purpose Integration", 2],
+        ["Co oznacza skrót CSS w kontekście programowania stron internetowych?", "Cascading Style Sheets", "Computer System Syntax", "Coding Style Standards", "Centralized Software Storage", 1],
+        ["Jaki język programowania jest najczęściej stosowany w analizie danych i obliczeniach naukowych?", "JavaScript", "R", "PHP", "Swift", 2],
+        ["Który język programowania jest powszechnie(najczęściej) stosowany w tworzeniu sztucznej inteligencji i uczenia maszynowego?", "C++", "Java", "Python", "Perl", 3],
+        ["Co oznacza skrót SQL w kontekście programowania baz danych?", "Structured Query Language", "Secure Query Logic", "Server Quality Link", "System Query Library", 1],
+        ["Który język programowania jest często wykorzystywany w tworzeniu gier komputerowych?", "Python", "Java", "C++", "Ruby", 3],
+        ["Co to jest pętla w programowaniu i jakie są jej rodzaje?", "Pętla to struktura sterująca, która umożliwia wielokrotne wykonanie określonych instrukcji.", "Pętla to zmienna przechowująca wartość.", "Pętla to funkcja matematyczna.", "Pętla to klasa w programowaniu obiektowym.", 1],
+        ["Co to jest zmienna w programowaniu?", "Zmienna to komentarz w kodzie programu.", "Zmienna to funkcja matematyczna.", "Zmienna to symbol reprezentujący wartość, którą można modyfikować.", "Zmienna to nazwa programu.", 3],
+        ["Co to jest metoda w programowaniu?", "To zorganizowany blok kodu, wykonujący określone zadanie, wywoływany w różnych częściach programu.", "Metoda to interfejs użytkownika.", "Metoda to klasa w programowaniu.", "Metoda to zmienna przechowująca dane.", 1],
+    ]
+
+    let geography = [
+        ["Który kontynent jest największy pod względem powierzchni?", "Afryka", "Azja", "Ameryka Południowa", "Europa", 2],
+        ["Ile kilometrów ma najdłuższa rzeka na świecie, Nil?", "6650 km", "8740 km", "5436 km", "7260 km", 1],
+        ["Który kraj jest największym producentem kawy na świecie?", "Brazylia", "Kolumbia", "Włochy", "Etiopia", 1],
+        ["Rzeka przepływająca przez stolice Francji to?", "Severn", "Loara", "Tamiza", "Sekwana", 4],
+        ["Gdzie znajduje się Wielka Rafa Koralowa?", "Morze Czerwone", "Morze Śródziemne", "Ocean Indyjski", "Ocean Spokojny", 4],
+        ["Który kontynent ma największą liczbę krajów?", "Europa", "Azja", "Afryka", "Ameryka Południowa", 1],
+        ["Jaka jest największa wyspa na świecie?", "Madagaskar", "Kuba", "Borneo", "Grenlandia", 4],
+        ["Który kraj jest znany jako Kraina Wschodzącego Słońca?", "Chiny", "Indie", "Japonia", "Brazylia", 3],
+        ["Gdzie znajduje się najniżej położone miejsce na Ziemi, Rów Mariański?", "Ocean Atlantycki", "Ocean Spokojny", "Morze Śródziemne", "Morze Martwe", 2],
+        ["Z którym krajem Polska ma najdłuższą granicę?", "Słowacja", "Niemcy", "Czechy", "Ukraina", 3],
+    ]
+
+    let history = [
+        ["Które wydarzenie rozpoczęło I wojnę światową?", "Atak na Pearl Harbor", "Zamach w Sarajewie", "Wojna secesyjna", "Rewolucja październikowa", 2],
+        ["Który przywódca rządził Niemcami podczas II wojny światowej?", "Benito Mussolini", "Joseph Stalin", "Winston Churchill", "Adolf Hitler", 4],
+        ["Które państwo było pierwsze, które wysłało człowieka na Księżyc?", "Stany Zjednoczone", "Rosja (ZSRR)", "Chiny", "Wielka Brytania", 1],
+        ["Które wydarzenie było przyczyną wybuchu wojny domowej w Stanach Zjednoczonych?", "Zamach na Johna F. Kennedy'ego", "Zrzucenie bomby atomowej na Hiroszimę", "Atak na Pearl Harbor", "Atak na Fort Sumter", 4],
+        ["Który rok oznaczał koniec zimnej wojny?", "1989", "1991", "1975", "1962", 2],
+        ["Kto był pierwszym prezydentem Stanów Zjednoczonych?", "George Washington", "Abraham Lincoln", "Thomas Jefferson", "Benjamin Franklin", 1],
+        ["Który historyczny dokument proklamował niezależność Stanów Zjednoczonych od Wielkiej Brytanii?", "Konstytucja Stanów Zjednoczonych", "Deklaracja Niepodległości", "Akt Unii", "Deklaracja Praw Człowieka i Obywatela", 2],
+        ["Które wydarzenie zapoczątkowało Rewolucję Francuską?", "Atak na Bastylię", "Wybuch wulkanu Wezuwiusz", "Wojna stuletnia", "Zamach na Ludwika XVI", 1],
+        ["Które wydarzenie rozpoczęło II wojnę światową w Europie?", "Atak na Pearl Harbor", "Atak na Polskę", "Zajęcie Czechosłowacji przez nazistowskie Niemcy", "Atak na Francję", 2],
+        ["Kto był ostatnim przywódcą Związku Socjalistycznych Republik Radzieckich", "Józef Stalin", "Włodzimierz Lenin", "Władimir Putin", "	Michaił Gorbaczow", 4],
+    ]
+
+    let math = [
+        ["Jaki jest wynik mnożenia 7 przez 9?", "56", "63", "72", "81", 2],
+        ["Jakie jest pole kwadratu o boku o długości 5 cm?", "10 cm²", "15 cm²", "20 cm²", "25 cm²", 4],
+        ["Jakie jest pole prostokąta o długości 8 cm i szerokości 3 cm?", "11 cm²", "21 cm²", "24 cm²", "32 cm²", 4],
+        ["Jakie jest pole trójkąta o podstawie o długości 6 cm i wysokości 4 cm?", "8 cm²", "12 cm²", "16 cm²", "24 cm²", 2],
+        ["Jakie jest pole koła o promieniu 5 cm? (przyjmij wartość liczby π jako 3.14)", "15.7 cm²", "25 cm²", "31.4 cm²", "78.5 cm²", 3],
+        ["Jakie jest pierwiastek kwadratowy z liczby 25?", "5", "10", "15", "20", 1],
+        ["Jakie jest wynik dodawania 3/4 + 1/2?", "1/8", "5/8", "7/8", "1 1/4", 2],
+        ["Jakie jest wynik odejmowania 12 - 7?", "3", "5", "7", "12", 2],
+        ["Jakie jest iloczyn 2/3 i 3/5?", "1/5", "2/5", "1", "6/15", 4],
+        ["Jakie jest wynik dzielenia 18 przez 6?", "2", "3", "6", "9", 2],
+    ]
+
+    let books = [
+        ["Kto jest autorem powieści Zbrodnia i kara?", "Fiodor Dostojewski", "Leo Tolstoj", "Virginia Woolf", "Franz Kafka", 1],
+        ["Która z poniższych książek nie została napisana przez J.R.R. Tolkiena?", "Władca Pierścieni", "Pierścienie Władzy", "Hobbit", "Silmarillion", 2],
+        ["Która powieść została napisana przez George'a Orwella?", "Wielki Gatsby", "Rok 1984", "Zapiski stalkerów", "Władca much", 2],
+        ["Kto jest autorem powieści Harry Potter?", "J.R.R. Tolkien", "J.K. Rowling", "C.S. Lewis", "Stephen King", 2],
+        ["Która z poniższych powieści została napisana przez Harukiego Murakamiego?", "Sto lat samotności", "W pustyni i w puszczy", "Norwegian Wood", "Złodziejka książek", 3],
+        ["Która powieść Agathy Christie przedstawia przygody detektywa Hercule Poirot?", "Dziesięciu murzynków", "Zaklinaczka dzieci", "Zbrodnia na polu golfowym", "Zasady zachowań przy stole", 3],
+        ["Kto jest autorem powieści Wielki Gatsby?", "F. Scott Fitzgerald", "Ernest Hemingway", "Mark Twain", "Oscar Wilde", 1],
+        ["Która z poniższych powieści nie należy do serii Cztery pory roku Stephena Kinga?", "Skazani na Shawshank", "Zielona Mila", "To", "Lśnienie", 3],
+        ["Kto jest autorem powieści Mały Książę?", "Antoine de Saint-Exupéry", "Albert Camus", "Gustave Flaubert", "Victor Hugo", 1],
+        ["Która powieść Emily Brontë opowiada historię Heathcliff i Cathy?", "Opowieść wigilijna", "Wichrowe Wzgórza", "Dziwne losy Jane Eyre", "Charlotte Brontë i jej siostry śpiące", 2],
+    ]
+
+    let game = [
+        ["Która gra z serii The Legend of Zelda zadebiutowała na konsoli Nintendo Switch?", "The Legend of Zelda: Ocarina of Time", "The Legend of Zelda: Breath of the Wild", "The Legend of Zelda: Skyward Sword", "The Legend of Zelda: Twilight Princess", 2],
+        ["Która gra z serii Assassins Creed osadzona jest w czasach starożytnego Egiptu?", "Assassin's Creed: Origins", "Assassin's Creed: Odyssey", "Assassin's Creed: Unity", "Assassin's Creed: Syndicate", 1],
+        ["Która część gry z serii Grand Theft Auto rozgrywa się w fikcyjnym mieście Los Santos?", "GTA III", "GTA Vice City", "GTA San Andreas", "GTA V", 4],
+        ["Która gra z serii Call of Duty osadzona jest w czasach II wojny światowej?", "Call of Duty: Modern Warfare", "Call of Duty: Black Ops", "Call of Duty: WWII", "Call of Duty: Advanced Warfare", 3],
+        ["Która gra z serii Final Fantasy jest uważana za klasyczną odsłonę serii i została wydana w 1997 roku?", "Final Fantasy VII", "Final Fantasy VIII", "Final Fantasy X", "Final Fantasy XII", 1],
+        ["Która gra z serii Pokémon jest pierwszą odsłoną serii wydaną na konsolę Nintendo Switch?", "Pokémon Red/Blue", "Pokémon Gold/Silver", "Pokémon Let's Go, Pikachu!", "Pokémon Sword/Shield", 3],
+        ["Która gra z serii The Elder Scrolls osadzona jest w krainie Skyrim?", "The Elder Scrolls III: Morrowind", "The Elder Scrolls IV: Oblivion", "The Elder Scrolls V: Skyrim", "The Elder Scrolls Online", 3],
+        ["", "", "", "", "", 2],
+        ["", "", "", "", "", 2],
+        ["", "", "", "", "", 2],
+    ]
+
+    ["", "", "", "", "", 2],
+
+
+Która gra z serii "Mass Effect" jest pierwszą odsłoną serii?
+a) "Mass Effect 2"
+b) "Mass Effect 3"
+c) "Mass Effect: Andromeda"
+d) "Mass Effect"
+Poprawna odpowiedź: d) "Mass Effect"
+    
+
+Która gra z serii "The Witcher" jest najnowszą odsłoną serii, wydaną w 2020 roku?
+a) "The Witcher: Enhanced Edition"
+b) "The Witcher 2: Assassins of Kings"
+c) "The Witcher 3: Wild Hunt"
+d) "The Witcher: Monster Slayer"
+Poprawna odpowiedź: c) "The Witcher 3: Wild Hunt"
+
+Która gra z serii "Super Mario" została wydana na konsolę Nintendo 64?
+a) "Super Mario Bros."
+b) "Super Mario Sunshine"
+c) "Super Mario 64"
+d) "Super Mario Galaxy"
+Poprawna odpowiedź: c) "Super Mario 64"
+
+
+-----------------------
+Który film zdobył Oscara dla najlepszego filmu w 2020 roku?
+a) "Parasite"
+b) "Joker"
+c) "1917"
+d) "Green Book"
+Poprawna odpowiedź: a) "Parasite"
+
+Który film jest adaptacją powieści Stephena Kinga i opowiada historię grupy dzieci walczących z przerażającym klaunem?
+a) "The Shining"
+b) "It"
+c) "Misery"
+d) "Carrie"
+Poprawna odpowiedź: b) "It"
+
+Który film jest pierwszą odsłoną sagi "Władca Pierścieni"?
+a) "Drużyna Pierścienia"
+b) "Dwie wieże"
+c) "Powrót króla"
+d) "Hobbit"
+Poprawna odpowiedź: a) "Drużyna Pierścienia"
+
+Który film został wyreżyserowany przez Christophera Nolana i opowiada historię złodzieja snów?
+a) "Incepcja"
+b) "Mroczny Rycerz"
+c) "Interstellar"
+d) "Dunkierka"
+Poprawna odpowiedź: a) "Incepcja"
+
+Który film z serii Marvel Cinematic Universe przedstawia historię superbohatera o mocy transformacji w zielonego potwora?
+a) "Iron Man"
+b) "Thor"
+c) "The Incredible Hulk"
+d) "Captain America: The First Avenger"
+Poprawna odpowiedź: c) "The Incredible Hulk"
+
+Który film zdobył rekordową liczbę 11 Oscarów, włączając w to najlepszy film, w 1997 roku?
+a) "Titanic"
+b) "Forrest Gump"
+c) "Ben-Hur"
+d) "The Lord of the Rings: The Return of the King"
+Poprawna odpowiedź: a) "Titanic"
+
+Który film opowiada historię młodego chłopca, który przypadkowo trafia do magicznej szkoły czarodziejów?
+a) "Harry Potter and the Philosopher's Stone"
+b) "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe"
+c) "Percy Jackson & the Olympians: The Lightning Thief"
+d) "The Spiderwick Chronicles"
+Poprawna odpowiedź: a) "Harry Potter and the Philosopher's Stone"
+
+Który film zdobył Oscara dla najlepszego filmu animowanego w 2020 roku?
+a) "Toy Story 4"
+b) "Spider-Man: Into the Spider-Verse"
+c) "Frozen II"
+d) "Soul"
+Poprawna odpowiedź: d) "Soul"
--- src/Components/Categories/CategoriesBtns.jsx
+++ src/Components/Categories/CategoriesBtns.jsx
@@ -15,7 +15,7 @@ function CategoriesBtns(props) {
             <Link to="/quiz/geography" onClick={categoryNameHandler}><button id="geography" className='btn3'><span role="img" aria-label="world" className="icon">🌐</span>Geografia</button></Link>
             <Link to="/quiz/history" onClick={categoryNameHandler}><button id="history" className='btn4'><span role="img" aria-label="bow" className="icon">🏹</span>Historia</button></Link>
             <Link to="/quiz/math" onClick={categoryNameHandler}><button id="math" className='btn4'><span role="img" aria-label="plus" className="icon">➕</span>Matematyka</button></Link>
-            <Link to="/quiz/books" onClick={categoryNameHandler}><button id="books" className='btn2'><span role="img" aria-label="book"  className="icon">📖</span>Lektury</button></Link>
+            <Link to="/quiz/books" onClick={categoryNameHandler}><button id="books" className='btn2'><span role="img" aria-label="book"  className="icon">📖</span>Książki</button></Link>
             <Link to="/quiz/games" onClick={categoryNameHandler}><button id="games" className='btn1'><span role="img" aria-label="game"  className="icon">🎮</span>Gry</button></Link>
             <Link to="/quiz/movie" onClick={categoryNameHandler}><button id="movie" className='btn3'><span role="img" aria-label="popcorn"  className="icon">🍿</span>Filmy</button></Link>
         </div>  
